c2d67adab26537faaeaabfb3e02fbfdc7e59295a
arunbdevadiga <77329454+arunbdevadiga@users.noreply.github.com>
2021-01-12 17:46:18 +0530

    Add files via upload

---
+++ d1assignment.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment-1
+
+# # Q-1 Find the value of the following questions where x=5
+
+# In[1]:
+
+
+x=int(input("enter value of x:"))
+y=(2*x+5/x**2+5*x+6)
+print("result is:",y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+x=int(input("enter value of x:"))
+y=(x**2+5*x+6/2*x+5)
+print("result is:",y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+x=int(input("enter value of x:"))
+y=((2*x-3)*(x-9))
+print("result is:",y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # Create a username and password login using nested while loop
+
+# In[9]:
+
+
+username = 'letsupgrade'
+password = 'letsup'
+userInput= input("enter usernaame")
+if userInput == username:
+    userInput = input("Password?")   
+    if userInput == password:
+       print("Welcome!")
+    else:
+       print("That is the wrong password.")
+else:
+    print("That is the wrong username.")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
